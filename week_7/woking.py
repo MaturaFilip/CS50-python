486ea804c6bf49e09a6b62a7373b092355e940c0
MaturaFilip <matura.filip96@gmail.com>
2025-03-07 18:37:45 +0100

    progress for problem 3 week 7 added

--- week_7/woking.py
+++ week_7/woking.py
@@ -3,11 +3,14 @@ import sys
 
 
 def main():
-    print(convert(input("Hours: ")))
+    print(convert("9 AM to 5 PM"))
+    #print(convert(input("Hours: ")))
 
 
 def convert(s):
-    ...
+    pattern = re.search(r"\d{2} (AM|PM) to \d{2} (PM|AM)")
+    if pattern:
+        ...groups()
 
 
 ...
